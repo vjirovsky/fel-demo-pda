d6cf5494653a35598b66f15489b21995d044bdac
VJirovsky <vjirovsky@vjirovsky.cz>
2014-10-15 23:22:36 +0200

    added universal app development demo

---
+++ MyUniversalApp/MyUniversalApp/MyUniversalApp.Shared/MyTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyUniversalApp
+{
+    class MyTimeProvider
+    {
+        public static string GetMyTime()
+        {
+            return "Time from shared time provider: " + DateTime.Now.ToString();
+        }
+    }
+}
---
+++ MyUniversalApp/MyUniversalApp/MyUniversalApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MyUniversalApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += updateText_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+        }
+
+        public void updateText_Tick(object sender, object e)
+        {
+            MyTextBox.Text = MyTimeProvider.GetMyTime();
+        }
+
+    }
+}
